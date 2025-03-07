dc0c23c7e248418ecf28890c038bc51a67267f98
Civodel <tucanbarrera@live.com.mx>
2025-03-06 20:34:07 -0600

    base url impv

--- src/database/db.py
+++ src/database/db.py
@@ -1,5 +1,3 @@
-import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -8,7 +6,7 @@ from src.database.models import Base
 # SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER', 'root')}:{os.getenv('MYSQL_PASSWORD', 'pass123')}@localhost:3306/{os.getenv('MYSQL_DB', 'kopichallengedb')}"
 
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:pass123@db:3306/kopichallengedb"
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = 'mysql://vq87a2wc0jge3nyt:hidw10hcb40ds5km@nnmeqdrilkem9ked.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/kkjc39umeunk66pp'
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
