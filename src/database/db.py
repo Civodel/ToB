d36dee69e41fea7740648f11db294edd5131a390
Civodel <tucanbarrera@live.com.mx>
2025-03-06 20:46:15 -0600

    base url impv

--- src/database/db.py
+++ src/database/db.py
@@ -1,8 +1,10 @@
+import pymysql
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from src.database.models import Base
 
+pymysql.install_as_MySQLdb()
 # SQLALCHEMY_DATABASE_URL = f"mysql+mysqlconnector://{os.getenv('MYSQL_USER', 'root')}:{os.getenv('MYSQL_PASSWORD', 'pass123')}@localhost:3306/{os.getenv('MYSQL_DB', 'kopichallengedb')}"
 
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:pass123@db:3306/kopichallengedb"
